5c052571a818b56667b4fef3794102fce6ede70c
Yoselin Diaz <dyoselin213@gmail.com>
2026-05-25 19:52:17 -0600

    Conectar Railway MySQL

--- helpcenter-backend/src/config/db.js
+++ helpcenter-backend/src/config/db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
