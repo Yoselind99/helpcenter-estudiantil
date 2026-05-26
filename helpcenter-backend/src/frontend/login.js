2429293fc1c116c36d2c8a03e1b7466a70eb15c9
Yoselin Diaz <dyoselin213@gmail.com>
2026-05-25 18:40:19 -0600

    Fix login URL

--- helpcenter-backend/src/frontend/login.js
+++ helpcenter-backend/src/frontend/login.js
@@ -1,4 +1,4 @@
-const API_LOGIN = " https://helpcenter-estudiantil.onrender.com/auth/login";
+const API_LOGIN = "https://helpcenter-estudiantil.onrender.com/auth/login";
 
 const formLogin = document.getElementById("formLogin");
 
